913be42697390d9814267e9dc62e9c8fd0c94e16
mrchidubem <mrchidubem8@gmail.com>
2025-09-02 09:47:49 +0100

    Register new models in admin for visibility

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import UserProfile, Budget, Expense, BudgetCategory, BudgetTemplate
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "country_code", "currency_symbol")
+    search_fields = ("user__username", "country_code")
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ("user", "goal_amount")
+    search_fields = ("user__username",)
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "description", "date")
+    search_fields = ("user__username", "description")
+    list_filter = ("date",)
+
+
+@admin.register(BudgetCategory)
+class BudgetCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "icon", "color")
+    search_fields = ("name",)
+
+
+@admin.register(BudgetTemplate)
+class BudgetTemplateAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    search_fields = ("name",)
